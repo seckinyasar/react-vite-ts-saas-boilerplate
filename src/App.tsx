881cdaf938e5139283bb220c2156771e27940fd0
seckin <contactseckin@gmail.com>
2024-07-25 18:55:58 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/index.scss";
+
+function App() {
+  return (
+    <>
+      <div className="container">Hello</div>
+    </>
+  );
+}
+
+export default App;
